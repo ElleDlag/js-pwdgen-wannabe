8e00a666cc53f7b14d90b30cad4cfa5d73daa046
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-16 17:30:34 +0200

    js variable prompt

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var resultHere = document.querySelector(".prompt_result-text");
+var name = prompt("Nome");
+var surname = name + prompt("Cognome");
+var color = surname + prompt("Il tuo colore preferito?");
+var result = color + (2019 - prompt("inserisci la data di nascita o l'età"));
+
+resultHere.innerHTML = result + "<div class='verify'><input type='checkbox' id='view-result'><label for='view-result'><i class='fas fa-check'></i>verify</label><div class='meter-value'></div></div>"
+
+
+
+
+
